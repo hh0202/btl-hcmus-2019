d00ad2eabac84a77ebbd5a6b281fd3c3d63b2cfa
hh0202 <lhoanghieu02@gmail.com>
2025-02-16 10:39:27 +0700

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,7 +1,8 @@
 #include "shit.h" 
 
+
 int main() {
-    const string filename = "tnc_tc_01_input";
+    const string filename = "input.txt";
 
     int LF1[17], LF2[17];
     int EXP1, EXP2, T1, T2, E;
@@ -14,10 +15,26 @@ int main() {
     {
         cout << "Failed to read the file.\n";
     }
-
-    int S = 0;
-    S = gatherForces(LF1, LF2);
-    cout << S;
-
+    cout << "Task 0: ...\n";
+    cout << "[";
+    for (int i = 0; i < 16; i++) {
+        cout << LF1[i] << ",";
+    }
+    cout << LF1[16] << "]\n";
+    cout << "[";
+    for (int i = 0; i < 16; i++) {
+        cout << LF2[i] << ",";
+    }
+    cout << LF2[16] << "]\n";
+    cout << EXP1 << " " << EXP2 << endl;
+    cout << T1 << " " << T2 << endl;
+    cout << E << endl;
+    cout << "Task 1: ...\n";
+    int c = gatherForces(LF1, LF2);
+    cout << c << endl;
+    cout << "Task 2.1: ...\n";
+    string str1 = "2skibidi";
+    string str2 = determineRightTarget(str1);
+    cout << str2 << endl;
     return 0;
 }
